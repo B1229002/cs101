16056ca7b7619b77be9ed7cd9b09cc29fe9a4c4f
B1229002 <145583526+B1229002@users.noreply.github.com>
2023-12-17 22:33:41 +0800

    Update main3.c

--- hw5/main3.c
+++ hw5/main3.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
-int main(){
-    for(int i=1;i<=9;i++){
-        for(int j=1;j<=9;j++){
-            int ans=i*j;
-            printf("%d*%d=%d\t",i,j,ans);
+    int main(){
+    for(int i=1, j=1;i<=9;j++){
+        int ans=i*j;
+        printf("%d*%d=%d\t",i,j,ans);
+        //j==9 的時候換行 才不會跑到數字10
+        if (j==9){
+            printf("\n");
+            //從一開始乘
+            j=0;
+            i++;
         }
-        printf("\n");
+
     }
     
 }
